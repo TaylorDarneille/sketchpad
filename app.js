537c44977add506024e6b1414c2ff34caa7e3900
Taylor Darneille <tmdarneille@gmail.com>
2020-09-16 13:18:17 -0700

    for class reference

---
+++ app.js
@@ -0,0 +1,24 @@
+const changeColor = (e) => {
+    // e.target.classList.add('pink')
+    const red = Math.floor(Math.random()*255)
+    const green = Math.floor(Math.random()*255)
+    const blue = Math.floor(Math.random()*255)
+    e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+}
+
+const generateSketchpad = (num) =>{
+    for (let i = 0; i < num; i++) {
+		let div = document.createElement('div')
+        div.classList.add('square')
+        div.addEventListener('mouseover', changeColor)
+		document.querySelector('.container').appendChild(div)
+	}
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const inputBtn = document.getElementById('inputButton')
+    const inputBox = document.getElementById('inputBox')
+    inputBtn.addEventListener('click', ()=>{
+        generateSketchpad(inputBox.value)
+    })  
+})
